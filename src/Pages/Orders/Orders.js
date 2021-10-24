207622c9f7f8a4b26e533b75586365190bd86b08
Midhun <midun.kr@inapp.com>
2021-10-24 23:10:37 +0530

    minor change in orders screen

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -5,6 +5,13 @@ import { Container, Row, Col, Button } from "react-bootstrap";
 
 export default function Orders(){
     const orderContext=useContext(OrderContext);
+    if (orderContext.orders.length == 0) {
+        return (
+          <Container className="d-flex" fluid>
+            <h3 className="justify-content-center m-2">No Orders Yet!!!!</h3>
+          </Container>
+        )
+      }
     return(
         <div>
             <Container>
